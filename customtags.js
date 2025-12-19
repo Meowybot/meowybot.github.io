da09a8e7a4d350818c740abe51ca679d9270db78
Meowybot <jorgedidier149@gmail.com>
2025-12-19 16:09:15 -0600

    Update meowy-pagenav

--- customtags.js
+++ customtags.js
@@ -1,8 +1,10 @@
 class MeowyPagesNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <nav>
-      <a href="/">Main Page</a>
+      <nav style="background-color: #222222;">
+      <a href="/" style="color: white;">Main Page</a>
+      <a href="/games" style="color: white;">My Games</a>
+      <a href="/csstest" style="color: white;">CSS Tests</a>
       </nav>
     `;
   }
