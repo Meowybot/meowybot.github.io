2fe1f5368519233e9997ee91d802084511d05986
Meowybot <jorgedidier149@gmail.com>
2026-01-05 14:38:38 -0600

    Update customtags.js

--- customtags.js
+++ customtags.js
@@ -4,10 +4,10 @@ class MeowyPagesNav extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <nav style="background-color: #222222;">
-      <a href="/" style="color: white;">Main Page</a>
-      <a href="/games" style="color: white;">My Games</a>
-      <a href="/csstest" style="color: white;">CSS Tests</a>
-      <a href="/oldA" style="color: white;">first version</a>
+      <a href="/" style="color: white;">Main Page</a> xd
+      <a href="/games" style="color: white;">My Games</a> xd
+      <a href="/csstest" style="color: white;">CSS Tests</a> xd
+      <a href="/oldA" style="color: white;">first version</a> xd
       </nav>
     `;
   }
